2d2d5c83e0208170b2f6bef1e46f9571ebc88201
koalalovepabro <koalalovepabro@gmail.com>
2021-03-30 12:04:36 +0900

    sql 연습2

--- sql-practices/01. basic-sql-practices.sql
+++ sql-practices/01. basic-sql-practices.sql
@@ -10,3 +10,27 @@ death date
 
 -- table scheme 확인
 desc pets;
+
+-- insert
+ insert
+   into pets
+ values('성탄이', 'kickscar', 'dog', 'm', '2010-12-25', null);
+ 
+ insert
+   into pets(owner, name, species, gender, birth)
+ values('kickscar', 'choco', 'cat', 'm', '2015-01-01');
+
+ insert
+   into pets
+ values('마음이', 'kickscar', 'dog', 'm', '2012-12-25', '2021-03-30');
+
+
+-- select
+select * from pets;
+
+select name, birth from pets;
+
+select name, birth from pets order by birth asc;
+
+select count(*) from pets;
+select count(*) from pets where death is not null;
